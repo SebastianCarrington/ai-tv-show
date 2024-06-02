06a58e99abf0498d33c5d011273bf3c707c64a65
SebastianCarrington <sebastiancarrington.music@gmail.com>
2024-06-03 00:21:37 +0100

    added websocket support

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import * as mongo from "./managers/mongo-manager.js";
 import * as story from "./managers/story-manager.js";
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    ws.send("lol");
+  });
+});
 
 /*mongo.addTopic("patrick has discovered that he can only say the word shit", [
   "spongebob",
   "patrick",
 ]);*/
-
-const script = await story.generateScript();
-mongo.sendCompletedScript(script.title, script.lines);
