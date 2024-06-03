78541470c8333e2a8306966afbe8afe1789a1f03
SebastianCarrington <64775211+SebastianCarrington@users.noreply.github.com>
2024-06-03 16:08:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ import * as story from "./managers/story-manager.js";
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
+let verified = false;
 
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
-    ws.send("lol");
+    if (data == "lol") {
+      verified = true;
+    }
+
+    if (verified) {
+      // topic handler goes in here
+    }
   });
 });
 
